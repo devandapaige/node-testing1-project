44a34781e8e49421e2e746be52586a712d69d370
Amanda Nelson <amanda.gaylord@gmail.com>
2021-03-24 16:26:44 -0400

    MVP pushing

--- index.test.js
+++ index.test.js
@@ -10,37 +10,54 @@ describe("[Exercise 1] trimProperties", () => {
   });
   it("[2] returns a copy, leaving the original object intact", () => {
     // ✨ test away
+    const input = { foo: "foo   ", bar: "bar   ", baz: "   baz" };
+    const actual = utils.trimProperties(input);
+    expect(actual).not.toEqual(input);
   });
 });
 
 describe("[Exercise 2] trimPropertiesMutation", () => {
   it("[3] returns an object with the properties trimmed", () => {
-    // ✨ test away
+    const input = { foo: "foo   ", bar: "bar   ", baz: "   baz" };
+    const expected = { foo: "foo", bar: "bar", baz: "baz" };
+    const actual = utils.trimPropertiesMutation(input);
+    expect(actual).toEqual(expected);
   });
   it("[4] the object returned is the exact same one we passed in", () => {
-    // ✨ test away
+    const input = { foo: "foo   ", bar: "bar   ", baz: "   baz" };
+    utils.trimPropertiesMutation(input);
+    expect(input).toEqual(input);
   });
 });
 
 describe("[Exercise 3] findLargestInteger", () => {
   it("[5] returns the largest number in an array of numbers", () => {
-    // ✨ test away
+    const input = [35, 21, 12, 1, 2, 50];
+    const result = utils.findLargestInteger(input);
+    expect(result).toBe(50);
   });
 });
 
 describe("[Exercise 4] Counter", () => {
   let counter;
   beforeEach(() => {
-    counter = new utils.Counter(3); // each test must start with a fresh couter
+    counter = new utils.Counter(3); // each test must start with a fresh counter
   });
   it("[6] the FIRST CALL of counter.countDown returns the initial count", () => {
     // ✨ test away
+    expect(counter.countDown()).toBe(3);
   });
   it("[7] the SECOND CALL of counter.countDown returns the initial count minus one", () => {
     // ✨ test away
+    counter.countDown();
+    expect(counter.countDown()).toBe(2);
   });
   it("[8] the count eventually reaches zero but does not go below zero", () => {
     // ✨ test away
+    for (let i = 0; i < 10; i++) {
+      counter.countDown();
+    }
+    expect(counter.countDown()).toBe(0);
   });
 });
 
@@ -50,22 +67,37 @@ describe("[Exercise 5] Seasons", () => {
     seasons = new utils.Seasons(); // each test must start with fresh seasons
   });
   it('[9] the FIRST call of seasons.next returns "summer"', () => {
-    // ✨ test away
+    expect(seasons.next()).toBe("summer");
   });
   it('[10] the SECOND call of seasons.next returns "fall"', () => {
-    // ✨ test away
+    for (let i = 0; i < 1; i++) {
+      seasons.next();
+    }
+    expect(seasons.next()).toBe("fall");
   });
   it('[11] the THIRD call of seasons.next returns "winter"', () => {
-    // ✨ test away
+    for (let i = 0; i < 2; i++) {
+      seasons.next();
+    }
+    expect(seasons.next()).toBe("winter");
   });
   it('[12] the FOURTH call of seasons.next returns "spring"', () => {
-    // ✨ test away
+    for (let i = 0; i < 3; i++) {
+      seasons.next();
+    }
+    expect(seasons.next()).toBe("spring");
   });
   it('[13] the FIFTH call of seasons.next returns again "summer"', () => {
-    // ✨ test away
+    for (let i = 0; i < 4; i++) {
+      seasons.next();
+    }
+    expect(seasons.next()).toBe("summer");
   });
   it('[14] the 40th call of seasons.next returns "spring"', () => {
-    // ✨ test away
+    for (let i = 0; i < 39; i++) {
+      seasons.next();
+    }
+    expect(seasons.next()).toBe("spring");
   });
 });
 
@@ -75,30 +107,41 @@ describe("[Exercise 6] Car", () => {
     focus = new utils.Car("focus", 20, 30); // each test must start with a fresh car
   });
   it("[15] driving the car returns the updated odometer", () => {
-    // ✨ test away
+    focus.drive(20);
+    expect(focus.odometer).toBe(20);
   });
   it("[16] driving the car uses gas", () => {
-    // ✨ test away
+    focus.drive(30);
+    expect(focus.tank).toBe(19);
   });
   it("[17] refueling allows to keep driving", () => {
-    // ✨ test away
+    focus.drive(300);
+    expect(focus.odometer).toBe(300);
+    focus.refuel(20);
+    focus.drive(100);
+    expect(focus.odometer).toBe(400);
   });
   it("[18] adding fuel to a full tank has no effect", () => {
-    // ✨ test away
+    focus.refuel(100);
+    expect(focus.tank).toBe(20);
   });
 });
 
 describe("[Exercise 7] isEvenNumberAsync", () => {
-  it("[19] resolves true if passed an even number", () => {
-    // ✨ test away
+  it("[19] resolves true if passed an even number", async () => {
+    const num = await utils.isEvenNumberAsync(2);
+    expect(num).toBe(true);
   });
-  it("[20] resolves false if passed an odd number", () => {
-    // ✨ test away
+  it("[20] resolves false if passed an odd number", async () => {
+    const num = await utils.isEvenNumberAsync(3);
+    expect(num).toBe(false);
   });
-  it('[21] rejects an error with the message "number must be a number" if passed a non-number type', () => {
-    // ✨ test away
+  it('[21] rejects an error with the message "number must be a number" if passed a non-number type', async () => {
+    const num = await utils.isEvenNumberAsync("string");
+    expect(num).toContain("number");
   });
-  it('[22] rejects an error with the message "number must be a number" if passed NaN', () => {
-    // ✨ test away
+  it('[22] rejects an error with the message "number must be a number" if passed NaN', async () => {
+    const num = await utils.isEvenNumberAsync(NaN);
+    expect(num).toContain("number");
   });
 });
